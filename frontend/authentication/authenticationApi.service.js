5b2e010acd3a4c0b98da7de0c092802f344f64a2
Andreas Frisch <andreas.frisch@gmail.com>
2015-10-22 16:16:28 +0100

    Added authentication to backend

--- frontend/authentication/authenticationApi.service.js
+++ frontend/authentication/authenticationApi.service.js
@@ -7,6 +7,7 @@ angular.module('authentication')
 		login: _login,
 		register: _register,
 		logout: _logout,
+        verify: _verify,
 	};
 	
 	function _login(username, password) {
@@ -19,7 +20,7 @@ angular.module('authentication')
 		.then(
 			function(responseData) {
 				console.log('authenticationApi > login successful: ', responseData.data);
-				sessionStorage.setItem('accessToken', responseData.data);
+				sessionStorage.setItem('accessToken', responseData.data.token);
 				authenticationStatus.isAuthenticated = true;
 				deferred.resolve(200);
 			},
@@ -56,4 +57,22 @@ angular.module('authentication')
 		sessionStorage.removeItem('accessToken');
 		authenticationStatus.isAuthenticated = false;
 	}
+    
+    function _verify() {
+        console.log('authenticationApi > verify key');
+        var deferred = $q.defer();
+        console.log('verifying');
+        $http.post('/api/auth/verify/', {})
+        .then(
+            function(responseData) {
+                console.log('authentication verification success');
+                deferred.resolve(200);
+            },
+            function(error) {
+                console.log('authentication verification failed: ', error);
+                deferred.reject(error.status);
+            }
+        );
+        return deferred.promise;
+    }
 }]);
