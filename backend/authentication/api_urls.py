0c5eddd5666e150ffa09cf3c70e4a935d2db347a
Andreas Frisch <andreas.frisch@gmail.com>
2015-10-28 16:32:07 +0000

    Fixed name spelling for authentication app

--- backend/authentication/api_urls.py
+++ backend/authentication/api_urls.py
@@ -5,7 +5,7 @@ URL handlers for authentication API
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns(
-    'backend.auth.api_views',
+    'backend.authentication.api_views',
     url(r'^login/', 'login'),
     url(r'^logout/', 'logout'),
     url(r'^verify/', 'verify'),
--- backend/urls.py
+++ backend/urls.py
@@ -12,7 +12,7 @@ urlpatterns = patterns(
     url(r'^$', 'backend.views.home', name='metube_home'),
     url(r'^api/', include([
         url(r'^blog/', include('backend.blog.api_urls')),
-        url(r'^auth/', include('backend.auth.api_urls'))
+        url(r'^auth/', include('backend.authentication.api_urls'))
     ])),
     url(r'^admin/', include(admin.site.urls)),
 )
