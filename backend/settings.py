3040602ab6029853b52ad6ea4fa990c940a2df16
Andreas Frisch <andreas.frisch@gmail.com>
2015-10-28 16:34:50 +0000

    Changed name of test-db

--- backend/settings.py
+++ backend/settings.py
@@ -66,7 +66,7 @@ TEMPLATE_DIRS = (
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'test.db'),
     }
 }
 
